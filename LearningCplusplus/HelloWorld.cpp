f7585f0f735bcb410f7bf62ed84bc4f4b08d8090
Ume-Chika <chikarameitian@gmail.com>
2025-07-28 17:48:00 +0900

    add ignore exe

---
+++ LearningCplusplus/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+    // コンソールに "Hello, World!" と出力します
+    std::cout << "Hello, World!" << std::endl; // 競プロで使う書き方, iostream版
+    printf("Hello, World!!\n"); // C言語っぽい書き方
+    return 0;
+}
